8a99b1b24237e76bdab1b4b53bebe6c4bc0310fa
angelocrispim <41017623+angelocrispim@users.noreply.github.com>
2026-06-22 20:24:22 -0300

    Configurando banco render

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from dotenv import load_dotenv
-import os
 
 load_dotenv()
 
@@ -17,15 +17,9 @@ SessionLocal = sessionmaker(
 
 Base = declarative_base()
 
-
 def get_db():
-
     db = SessionLocal()
-
     try:
         yield db
-
     finally:
         db.close()
-
-Base.metadata.create_all(bind=engine)
