4798f559fa8631df28eaa1c86190507f148b74fd
angelocrispim <41017623+angelocrispim@users.noreply.github.com>
2026-07-10 23:48:16 -0300

    Remove router pdv vazio

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ from routes.finance import router as finance_router
 from routes.cash_register import router as cash_register_router
 from routes import caixa
 from routes.caixa import router as caixa_router
-from routes.pdv import router as pdv_router
+
 
 from routes.employee import router as employee_router
 
